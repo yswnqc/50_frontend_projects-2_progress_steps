3c6958906a3faf9ec7cc7b32617ebc67791979b6
Jinling Han <hjl-990406@outlook.com>
2024-11-02 23:22:32 +1100

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-const cards = document.querySelectorAll('.card');
+const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prev-btn')
+const progressBar = document.querySelector('.progress-bar')
+const circles = document.querySelectorAll('.circle')
 
-cards.forEach(card => card.addEventListener('click', () => {
-  cards.forEach(card => card.classList.remove('active'));
-  card.classList.add('active');
-}));
+let currentIndex = 1;
 
-// fetch('https://cors-anywhere.herokuapp.com/http://m.wufazhuce.com/one')//会有cors问题！！这个网址限流，看看别的
-//   .then(res => res.text())
-//   .then(html => {
-//     COOKIE = res.headers.get('set-cookie') || '';
-//     TOKEN = html.match(/One.token = '([a-zA-Z0-9]{40})'/)?.[1] || '';
-    
-//     return fetch(`http://m.wufazhuce.com/one/ajaxlist/${page}?_token=${TOKEN}`, {
-//       headers: { 'Cookie': COOKIE }
-//     });
-//   })
-//   .then(res => res.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error(error));
+nextBtn.addEventListener('click', () => {
+  currentIndex++;
+  if (currentIndex > circles.length) {
+    currentIndex = circles.length
+  }
+  update();
+})
 
+prevBtn.addEventListener('click', () => {
+  currentIndex--;
+  if (currentIndex < 1) {
+    currentIndex = 1
+  }
+  update();
+})
 
-//想把「ONE 一个」接入这个小项目，就可以每天，有图，有好句
-//可以写个后端
-//设成自动定时展示下一个card
+const update = () => {
+  circles.forEach((circle, index) => {
+    if (index < currentIndex) {
+      circle.classList.add('delay')
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('delay')
+      circle.classList.remove('active')
+    }
+  })
+
+  const actives = document.querySelectorAll('.active')
+  progressBar.style.width =
+      (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+  if (currentIndex === 1) {
+    prevBtn.disabled = true
+  }
+  else if (currentIndex === circles.length) {
+    nextBtn.disabled = true
+  }
+  else {
+    prevBtn.disabled = false
+    nextBtn.disabled = false
+  }
+}
+
+//这个能有什么improve的地方吗？
